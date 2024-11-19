7194f532acb9ad8b34251dece7d66b677b6debaf
barmud3 <barmud9798@gmail.com>
2024-11-19 23:55:24 +0200

    update SmoothMover

--- Assets/SmoothMover.cs
+++ Assets/SmoothMover.cs
@@ -10,20 +10,52 @@ public class SmoothMover : MonoBehaviour
     [SerializeField]
     InputAction moveLeft = new InputAction(type: InputActionType.Button);
 
+    [SerializeField]
+    InputAction moveUp = new InputAction(type: InputActionType.Button);
+
+    [SerializeField]
+    InputAction moveDown = new InputAction(type: InputActionType.Button);
 
     [SerializeField]
     [Tooltip("How many meters per second to move when action is pressed.")]
     private float speed =1;
 
+        private void Awake()
+    {
+        // Auto-bind actions if no bindings are set
+        if (moveRight.bindings.Count == 0)
+        {
+            moveRight.AddBinding("<Keyboard>/d"); // Default: D key for moving right
+        }
+
+        if (moveLeft.bindings.Count == 0)
+        {
+            moveLeft.AddBinding("<Keyboard>/a"); // Default: A key for moving left
+        }
+
+        if (moveUp.bindings.Count == 0)
+        {
+            moveUp.AddBinding("<Keyboard>/w"); // Default: W key for moving up
+        }
+
+        if (moveDown.bindings.Count == 0)
+        {
+            moveDown.AddBinding("<Keyboard>/s"); // Default: S key for moving down
+        }
+    }
 
     private void OnEnable() {
         moveRight.Enable();
         moveLeft.Enable();
+        moveUp.Enable();
+        moveDown.Enable();
         
     }
     private void OnDisable() {
         moveRight.Disable();
         moveLeft.Disable();
+        moveUp.Disable();
+        moveUp.Disable();
     }
 
     // Update is called once per frame
@@ -35,6 +67,14 @@ public class SmoothMover : MonoBehaviour
 
         if(moveLeft.IsPressed()){
             transform.position += new Vector3(-speed * Time.deltaTime,0,0);
+        }
+
+        if(moveUp.IsPressed()){
+            transform.position += new Vector3(0,speed * Time.deltaTime,0);
+        }
+
+        if(moveDown.IsPressed()){
+            transform.position += new Vector3(0,-speed * Time.deltaTime,0);
         }
         
     }
