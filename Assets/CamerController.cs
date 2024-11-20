af07ed3bf69b9dd76d06aae86e78edfff640a5a9
barmud3 <barmud9798@gmail.com>
2024-11-20 08:51:08 +0200

    update first and second scenes , saifA saifB

--- Assets/CamerController.cs
+++ Assets/CamerController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
-public class CamerController : MonoBehaviour
+public class CameraScaler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Camera cam; // Reference to the Camera component
+    private float initialOrthographicSize; // The starting size of the orthographic camera
+    private float initialAspectRatio; // The initial aspect ratio of the screen (width/height)
+
     void Start()
     {
-        
+        // Get the Camera component
+        cam = GetComponent<Camera>();
+
+        // Save the initial orthographic size
+        initialOrthographicSize = cam.orthographicSize;
+
+        // Calculate and save the initial aspect ratio
+        initialAspectRatio = (float)Screen.width / Screen.height;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate the current aspect ratio
+        float currentAspectRatio = (float)Screen.width / Screen.height;
+
+        // Adjust the orthographic size to compensate for the aspect ratio change
+        cam.orthographicSize = initialOrthographicSize * (initialAspectRatio / currentAspectRatio);
     }
 }
