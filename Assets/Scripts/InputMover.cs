44010e0939d2a352e744bbd83575bf71dbd71b49
barmud3 <barmud9798@gmail.com>
2024-12-11 09:41:46 +0200

    split the game view to show the two cameras of each player

--- Assets/Scripts/InputMover.cs
+++ Assets/Scripts/InputMover.cs
@@ -11,7 +11,7 @@ public class InputMover : MonoBehaviour
     [SerializeField]
     InputAction move = new InputAction(
         type: InputActionType.Value, expectedControlType: nameof(Vector2));
-
+    
     void OnEnable()
     {
         move.Enable();
@@ -25,13 +25,15 @@ public class InputMover : MonoBehaviour
     void Update()
     {
         Vector2 moveDirection = move.ReadValue<Vector2>();
+        if (moveDirection.x < 0) // Moving left
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0); // Flip to face left
+        }
+        else if (moveDirection.x > 0) // Moving right
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0); // Face right
+        }
         Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
         transform.position += movementVector;
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
-        //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
-        // It makes a difference only if the object is rotated.
     }
 }
--- Assets/Scripts/Platform/GroundCheck.cs
+++ Assets/Scripts/Platform/GroundCheck.cs
@@ -1,17 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GroundCheck : MonoBehaviour
 {
     public bool grounded = false;
 
+    // Define an InputAction for jumping
+    [SerializeField] private InputAction jumpAction;
+
+    private Rigidbody2D rb;
+
+    private void OnEnable()
+    {
+        // Enable the input action when the object is enabled
+        jumpAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input action when the object is disabled
+        jumpAction.Disable();
+    }
+
+    private void Start()
+    {
+        // Cache the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody2D component is missing!");
+        }
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        // Check if the jump action is triggered and the player is grounded
+        if (jumpAction.triggered && grounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(transform.up * 10, ForceMode2D.Impulse);
+            Jump();
         }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
