117f607d1d852cdb80886ab93d2474662af6a7cb
ron_shuster <118674717+shusteron@users.noreply.github.com>
2024-12-11 07:58:02 +0200

    Update GroundCheck.cs
    for linter porpuse

--- Assets/Scripts/Platform/GroundCheck.cs
+++ Assets/Scripts/Platform/GroundCheck.cs
@@ -6,17 +6,21 @@ public class GroundCheck : MonoBehaviour
 {
     public bool grounded = false;
 
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            GetComponent<Rigidbody2D>().AddForce(transform.up * 10 , ForceMode2D.Impulse);
+            GetComponent<Rigidbody2D>().AddForce(transform.up * 10, ForceMode2D.Impulse);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         grounded = true;
     }
-    private void OnCollisionExit2D(Collision2D other) {
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
         grounded = false;
     }
 }
